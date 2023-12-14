f17299e433b37fd0a6752bc8a0bcbf01aa8b5532
roboticsw <wuzhong1@sina.com>
2023-12-14 23:16:11 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ source /app/config.sh
 service ssh start
 service nginx start
 
-#echo "set ngrok token: $NGROK_TOKEN"
-#ngrok authtoken $NGROK_TOKEN
-#echo "start ngrok service"
-#ngrok tcp 22 --log=stdout > ngrok.log
+echo "set ngrok token: $NGROK_TOKEN"
+ngrok authtoken $NGROK_TOKEN
+echo "start ngrok service"
+ngrok tcp 22 --log=stdout > ngrok.log
 /app/cpolar authtonken Y2U5MmU3M2MtZDdhMS00YTFmLTlkYzQtODlmZmExZjhlZDVm
 /app/cpolar tcp 22
